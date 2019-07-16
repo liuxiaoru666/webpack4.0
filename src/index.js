9222fd43afa06199908047ed9d857ae3baa50ffd
liuxiaoru <liuxiaoru@17shanyuan.com>
2019-07-16 18:52:17 +0800

    config

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
-import '@babel/polyfill';
 //全局样式
 import './static/css/common.scss';
 import './style.css';
-
 //引入图片
 import logo from './static/images/logo.jpg';
 
-import React,{Component} from 'react';
-import ReactDom from 'react-dom';
-class App extends Component {
-    render(){
-        return(
-            <div>Hello world</div>
-        )
-    }
-}
+import {add} from './com';
+console.log(add(1,2))
 
-ReactDom.render(<App/>,document.getElementById('root'));
--- webpack.base.js
+++ webpack.base.js
@@ -7,16 +7,6 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports =  {
-    mode:'development',//development时打包文件不压缩，production时压缩
-    devtool:'cheap-module-eval-source-map',//开发映射关系最佳实践
-    // devtool:'cheap-module-source-map',//线上最佳实践
-    devServer:{
-        contentBase:'./dist',
-        open:true,//autoOpenBroser
-        port:8080,
-        hot:true,//开启HMR模块热替换
-        hotOnly:true//浏览去不自动刷新
-    },
     entry:{
         main:'./src/index.js',
         // sub:'./src/index.js'//打包多个js
@@ -88,7 +78,6 @@ module.exports =  {
         filename:'index.html',
         template:'index.html'
         }),
-        new CleanWebpackPlugin(),
-        new webpack.HotModuleReplacementPlugin()
+        new CleanWebpackPlugin()
     ]
 }
